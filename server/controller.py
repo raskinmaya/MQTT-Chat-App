cdd1815ad9b387fe9a4330557fd9244ed2effb46
mayaraskin <mayaraskin14@gmail.com>
2025-05-23 16:45:36 +0300

    Added ValidationError handling on client topics

--- server/controller.py
+++ server/controller.py
@@ -1,21 +1,40 @@
+import json
+
 from paho.mqtt.client import Client, MQTTMessage
+from pydantic import ValidationError
+
+from common.logger import get_logger
 from common.messages import RegisterMessage, ChatMessage, LookupMessage
 from server.engine.utils import topic_handler
 from server.service import ClientService
 
 client_service = ClientService()
+logger = get_logger("Server:Controller")
 
 @topic_handler("register")
 def handle_register(parts: list[str], msg: MQTTMessage, client: Client) -> None:
-    data = RegisterMessage.model_validate_json(msg.payload)
-    client_service.register(data.username, data.address, client)
+    try:
+        data = RegisterMessage.model_validate_json(msg.payload)
+        client_service.register(data.username, data.address, client)
+    except ValidationError as e:
+        logger.error(f"Register validation error: {e}")
+        client.publish("error/register", json.dumps({"error": str(e)}))
 
 @topic_handler("send")
 def handle_send(parts: list[str], msg: MQTTMessage, client: Client) -> None:
-    message = ChatMessage.model_validate_json(msg.payload)
-    client_service.route_message(message.to_user, message.model_dump(), client)
+    try:
+        data = ChatMessage.model_validate_json(msg.payload)
+        client_service.route_message(data.to_user, data.model_dump(), client)
+    # TODO: Add user not found error
+    except ValidationError as e:
+        logger.error(f"Send validation error: {e}")
+        client.publish("error/send", json.dumps({"error": str(e)}))
 
 @topic_handler("lookup")
 def handle_lookup(parts: list[str], msg: MQTTMessage, client: Client) -> None:
-    data = LookupMessage.model_validate_json(msg.payload)
-    client_service.handle_lookup(data.requester, data.target, client)
+    try:
+        data = LookupMessage.model_validate_json(msg.payload)
+        client_service.handle_lookup(data.requester, data.target, client)
+    except ValidationError as e:
+        logger.error(f"Lookup validation error: {e}")
+        client.publish("error/lookup", json.dumps({"error": str(e)}))
