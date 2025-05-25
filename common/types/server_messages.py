6540012595e054afbaac6ea33cd08058196e3975
mayaraskin <mayaraskin14@gmail.com>
2025-05-26 00:26:18 +0300

    added Field(...) + removed optional from ServerError

--- common/types/server_messages.py
+++ common/types/server_messages.py
@@ -2,26 +2,27 @@ import time
 from typing import Optional
 from uuid import uuid4
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+
 
 class ServerMessage(BaseModel):
     request_id: Optional[str] = str(uuid4())
     timestamp: Optional[float] = time.time()
 
 class LookupResponse(ServerMessage):
-    target: str
-    address: str
+    target: str = Field(...)
+    address: str = Field(...)
 
 class ChatMessage(ServerMessage):
-    message: str
+    message: str = Field(...)
     content_base64: Optional[str] = ""
     filename: Optional[str] = ""
 
 class ServerAck(ServerMessage):
-    topic: str
-    username: str
+    topic: str = Field(...)
+    username: str = Field(...)
     address: Optional[str] = ""
     message: Optional[str] = ""
 
 class ServerError(ServerAck):
-    reason: Optional[str] = "An unexpected error occurred"
+    reason: str = Field(...)
